c9d0a7605e2c41857fa65cfcac1ae294cbd5a0aa
Pratyush Birole <birole.pratyush@gmail.com>
2024-06-22 18:54:09 +0530

    Initial React App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="w-full min-h-[100vh]">
+      <p className='text-blue'>Hello word</p>
+    
+    </div>
+  );
+}
+
+export default App;
