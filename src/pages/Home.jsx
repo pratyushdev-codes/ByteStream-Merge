f1ef70c6665749d2a3733a67c50787151054e9f1
Pratyush Birole <birole.pratyush@gmail.com>
2024-07-23 14:25:44 +0530

    Adding Profile Card

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
           <ProfileCard user={user}/>
         </div>
         {/*Center*/}       
-        <div></div>
+        <div className='flex-1 h-full px-4 flex flex-col gap-6 overflow-y-auto rounded-lg'> </div>
         {/*Right*/}       
         <div></div>
       </div>
